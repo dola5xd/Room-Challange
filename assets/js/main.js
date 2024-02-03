15fb69887c971267545c4826d78498c06cf5bcca
Dola5xd <dolay6253@gmail.com>
2024-02-03 18:43:59 +0200

    Edit Slide in Mobile and fix Mobile Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,12 +39,12 @@ const rightArrow = document.getElementById("right");
 
 leftArrow.addEventListener("click", () => {
   slideImg.style.opacity = 0;
-  slideImg.style.transform = "translateX(-100%)";
+  slideImg.style.transform = "translateX(100%)";
 
   slidehead.style.opacity = 0;
-  slidehead.style.transform = "translateX(-100%)";
+  slidehead.style.transform = "translateX(100%)";
   slidepara.style.opacity = 0;
-  slidepara.style.transform = "translateX(-100%)";
+  slidepara.style.transform = "translateX(100%)";
 
   setTimeout(() => {
     if (slideImg.alt == "hero-1") {
@@ -90,12 +90,12 @@ leftArrow.addEventListener("click", () => {
 
 rightArrow.addEventListener("click", () => {
   slideImg.style.opacity = 0;
-  slideImg.style.transform = "translateX(100%)";
+  slideImg.style.transform = "translateX(-100%)";
 
   slidehead.style.opacity = 0;
-  slidehead.style.transform = "translateX(100%)";
+  slidehead.style.transform = "translateX(-100%)";
   slidepara.style.opacity = 0;
-  slidepara.style.transform = "translateX(100%)";
+  slidepara.style.transform = "translateX(-100%)";
   setTimeout(() => {
     if (slideImg.alt == "hero-1") {
       slideImg.src = "./assets/images/mobile-image-hero-2.jpg";
@@ -144,12 +144,12 @@ document.addEventListener("keydown", (e) => {
   //   for right arrow to make evented
   if (e.key == "ArrowRight") {
     slideImg.style.opacity = 0;
-    slideImg.style.transform = "translateX(100%)";
+    slideImg.style.transform = "translateX(-100%)";
 
     slidehead.style.opacity = 0;
-    slidehead.style.transform = "translateX(100%)";
+    slidehead.style.transform = "translateX(-100%)";
     slidepara.style.opacity = 0;
-    slidepara.style.transform = "translateX(100%)";
+    slidepara.style.transform = "translateX(-100%)";
     setTimeout(() => {
       if (slideImg.alt == "hero-1") {
         slideImg.src = "./assets/images/mobile-image-hero-2.jpg";
@@ -192,12 +192,12 @@ document.addEventListener("keydown", (e) => {
   //   for left arrow to make evented
   else if (e.key == "ArrowLeft") {
     slideImg.style.opacity = 0;
-    slideImg.style.transform = "translateX(-100%)";
+    slideImg.style.transform = "translateX(100%)";
 
     slidehead.style.opacity = 0;
-    slidehead.style.transform = "translateX(-100%)";
+    slidehead.style.transform = "translateX(100%)";
     slidepara.style.opacity = 0;
-    slidepara.style.transform = "translateX(-100%)";
+    slidepara.style.transform = "translateX(100%)";
     setTimeout(() => {
       if (slideImg.alt == "hero-1") {
         slideImg.src = "./assets/images/mobile-image-hero-3.jpg";
@@ -252,15 +252,13 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-// Not best way
-slideImg.addEventListener("touchmove", (e) => {
-  var touch = e.targetTouches[0];
-  var midpoint = Math.floor(screen.width / 2);
-  console.log("midpoint: ", midpoint);
-  var px = touch.pageX;
-  console.log("px: ", px);
+// Make Mobiles slide with touchs
 
-  if (px < midpoint) {
+let touchstart = 0,
+  touchend = 0;
+
+function checkROL() {
+  if (touchend < touchstart) {
     slideImg.style.opacity = 0;
     slideImg.style.transform = "translateX(-100%)";
 
@@ -270,13 +268,58 @@ slideImg.addEventListener("touchmove", (e) => {
     slidepara.style.transform = "translateX(-100%)";
     setTimeout(() => {
       if (slideImg.alt == "hero-1") {
+        slideImg.src = "./assets/images/mobile-image-hero-2.jpg";
+        slideImg.alt = "hero-2";
+
+        slidehead.textContent = "We are available all across the globel";
+
+        slidepara.textContent = `With stores all over the world, it's easy for you to find furniture for your home or place of business.
+              Locally, we’re in most major cities throughout the country. Find the branch nearest you using our
+              store locator. Any questions? Don't hesitate to contact us today.`;
+      } else if (slideImg.alt == "hero-2") {
         slideImg.src = "./assets/images/mobile-image-hero-3.jpg";
         slideImg.alt = "hero-3";
 
         slidehead.textContent = "Manufactured with the best materials";
 
-        slidepara.textContent = `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
-          to ensure that every product is made as perfect and as consistent as possible. With three decades of 
+        slidepara.textContent = `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology
+                to ensure that every product is made as perfect and as consistent as possible. With three decades of
+                experience in this industry, we understand what customers want for their home and office.`;
+      } else if (slideImg.alt == "hero-3") {
+        slideImg.src = "./assets/images/mobile-image-hero-1.jpg";
+        slideImg.alt = "hero-1";
+
+        slidehead.textContent = "Discover innovative ways to decorate";
+
+        slidepara.textContent = `We provide unmatched quality, comfort, and style for property owners across the country.
+          Our experts combine form and function in bringing your vision to life. Create a room in your
+          own style with our collection and make your property a reflection of you and what you love.`;
+      }
+      slideImg.style.opacity = 1;
+      slideImg.style.transform = "translateX(0%)";
+
+      slidehead.style.opacity = 1;
+      slidehead.style.transform = "translateX(0%)";
+      slidepara.style.opacity = 1;
+      slidepara.style.transform = "translateX(0%)";
+    }, 500);
+  } else if (touchend > touchstart) {
+    slideImg.style.opacity = 0;
+    slideImg.style.transform = "translateX(100%)";
+
+    slidehead.style.opacity = 0;
+    slidehead.style.transform = "translateX(100%)";
+    slidepara.style.opacity = 0;
+    slidepara.style.transform = "translateX(100%)";
+    setTimeout(() => {
+      if (slideImg.alt == "hero-1") {
+        slideImg.src = "./assets/images/mobile-image-hero-3.jpg";
+        slideImg.alt = "hero-3";
+
+        slidehead.textContent = "Manufactured with the best materials";
+
+        slidepara.textContent = `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology
+          to ensure that every product is made as perfect and as consistent as possible. With three decades of
           experience in this industry, we understand what customers want for their home and office.`;
         //
       } else if (slideImg.alt == "hero-2") {
@@ -285,8 +328,8 @@ slideImg.addEventListener("touchmove", (e) => {
 
         slidehead.textContent = "Discover innovative ways to decorate";
 
-        slidepara.textContent = `We provide unmatched quality, comfort, and style for property owners across the country. 
-        Our experts combine form and function in bringing your vision to life. Create a room in your 
+        slidepara.textContent = `We provide unmatched quality, comfort, and style for property owners across the country.
+        Our experts combine form and function in bringing your vision to life. Create a room in your
         own style with our collection and make your property a reflection of you and what you love.`;
         //
       } else if (slideImg.alt == "hero-3") {
@@ -295,8 +338,8 @@ slideImg.addEventListener("touchmove", (e) => {
 
         slidehead.textContent = "We are available all across the globel";
 
-        slidepara.textContent = `With stores all over the world, it's easy for you to find furniture for your home or place of business. 
-        Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
+        slidepara.textContent = `With stores all over the world, it's easy for you to find furniture for your home or place of business.
+        Locally, we’re in most major cities throughout the country. Find the branch nearest you using our
         store locator. Any questions? Don't hesitate to contact us today.`;
         //
       }
@@ -308,50 +351,16 @@ slideImg.addEventListener("touchmove", (e) => {
       slidepara.style.opacity = 1;
       slidepara.style.transform = "translateX(0%)";
     }, 500);
-  } else {
-    slideImg.style.opacity = 0;
-    slideImg.style.transform = "translateX(100%)";
-
-    slidehead.style.opacity = 0;
-    slidehead.style.transform = "translateX(100%)";
-    slidepara.style.opacity = 0;
-    slidepara.style.transform = "translateX(100%)";
-    setTimeout(() => {
-      if (slideImg.alt == "hero-1") {
-        slideImg.src = "./assets/images/mobile-image-hero-2.jpg";
-        slideImg.alt = "hero-2";
-
-        slidehead.textContent = "We are available all across the globel";
-
-        slidepara.textContent = `With stores all over the world, it's easy for you to find furniture for your home or place of business. 
-          Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
-          store locator. Any questions? Don't hesitate to contact us today.`;
-      } else if (slideImg.alt == "hero-2") {
-        slideImg.src = "./assets/images/mobile-image-hero-3.jpg";
-        slideImg.alt = "hero-3";
-
-        slidehead.textContent = "Manufactured with the best materials";
-
-        slidepara.textContent = `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
-            to ensure that every product is made as perfect and as consistent as possible. With three decades of 
-            experience in this industry, we understand what customers want for their home and office.`;
-      } else if (slideImg.alt == "hero-3") {
-        slideImg.src = "./assets/images/mobile-image-hero-1.jpg";
-        slideImg.alt = "hero-1";
-
-        slidehead.textContent = "Discover innovative ways to decorate";
-
-        slidepara.textContent = `We provide unmatched quality, comfort, and style for property owners across the country. 
-      Our experts combine form and function in bringing your vision to life. Create a room in your 
-      own style with our collection and make your property a reflection of you and what you love.`;
-      }
-      slideImg.style.opacity = 1;
-      slideImg.style.transform = "translateX(0%)";
-
-      slidehead.style.opacity = 1;
-      slidehead.style.transform = "translateX(0%)";
-      slidepara.style.opacity = 1;
-      slidepara.style.transform = "translateX(0%)";
-    }, 500);
   }
+}
+
+slideImg.addEventListener("touchstart", (e) => {
+  touchstart = e.changedTouches[0].screenX;
+  console.log(e);
+  console.log(e.changedTouches[0]);
+});
+
+slideImg.addEventListener("touchend", (e) => {
+  touchend = e.changedTouches[0].screenX;
+  checkROL();
 });
